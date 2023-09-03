fba3778514baa283f01a4680be1e5d2fbdae2c0b
Marizza Real <realmarizza@gmail.com>
2023-09-03 03:12:26 -0700

    done

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ def main():
     females = input('Please enter number of females: ')
     total_stu = int(males) + int(females)
     m_perc = (int(males) / int(total_stu)) * 100
-    print(f'The percentage of males and females: \t {m_perc} \t {f_perc}') 
-                           
+    f_perc = (int(females) / int(total_stu)) *100
+    print(f'The percentage of males and females: \t {m_perc} \t {f_perc}')
+    print(total_stu)
+    print(m_perc + f_perc)
+    print (f'some message \t {f_perc:.2f}')                      
     """
     ########################################
     # Do not delete the return statement
